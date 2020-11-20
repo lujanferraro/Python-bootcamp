3a71a780a4c660f1e48f666c0f0d27f9e83a17be
Lujan Ferraro <lujan_ferraro@hotmail.com>
2020-11-20 18:43:39 -0300

    Ahorcado terminado

--- 3-Ahorcado.py
+++ 3-Ahorcado.py
@@ -1,8 +1,41 @@
+import time
+import random
+
+def hangman():
+    palabra = random.choice(["pequeño", "celular", "vaso", "pastilla", "caballo", "castuchera", "calculadora", "desodorante"])
+    intentos = 10
+    adivinando =""
+    while intentos > 0:
+        fallas = 0
+        for letra in palabra:
+             if letra in adivinando:
+                 print (letra, end="" )
+             else:
+                 print("-", end="" )
+                 fallas += 1
+        if fallas == 0:
+            print("Felicitaciones ganaste")
+            break
+        tuletra = input("Introduzca una letra: ")
+        adivinando += tuletra
+        if tuletra not in palabra:
+           intentos -=1
+           print ("Esa letra no esta")
+           print(" te quedan ",intentos," turnos")
+        if intentos == 0:
+            print ("Perdiste")
+    else:
+        print("Gracias por participar")
 
 
-name = input("Ingresa tu nombre")
-print("Bienvenid@", name)
+
+
+nombre = input("Ingresa tu nombre ")
+print("Bienvenid@", nombre)
 print("|||||||||||||||||||||||||||")
+time.sleep(1)
 print("Trata de adivinar la palabra en menos de 10 intentos")
+time.sleep(0.5)
+hangman()
 
 

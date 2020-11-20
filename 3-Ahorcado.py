73fd87ed31104125cef04c837d8f93cde548c746
Lujan Ferraro <lujan_ferraro@hotmail.com>
2020-11-20 15:24:50 -0300

    Comienzo ejercicio 3!

---
+++ 3-Ahorcado.py
@@ -0,0 +1,8 @@
+
+
+name = input("Ingresa tu nombre")
+print("Bienvenid@", name)
+print("|||||||||||||||||||||||||||")
+print("Trata de adivinar la palabra en menos de 10 intentos")
+
+
--- README.md
+++
@@ -1,5 +0,0 @@
-# Python-bootcamp
-Proyectos en Python del curso "Python Bootcamp 2020 Build 15 working Applications and Games"
-
-1- Simulador de un dado
-2- Diccionario
